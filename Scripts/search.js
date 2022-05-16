9c973408eff2ff6448069f75d3a670a279586ea8
Siddharth <officialsiddharthbisht@gmail.com>
2022-05-16 21:14:22 +0530

    partially done with the functionality of search page

--- Scripts/search.js
+++ Scripts/search.js
@@ -2,16 +2,35 @@ let searchBtn = document.getElementById("search");
 searchBtn.addEventListener('click',(event)=>{
     event.preventDefault();
     let input = document.getElementById("input").value;
-    let url = `https://api.openbrewerydb.org/breweries?by_name=${inut}&per_page=3`;
+    let url = `https://api.openbrewerydb.org/breweries?by_name=${input}&per_page=3`;
     fetch(url)
     .then((response)=>{
         return response.json();
     })
     .then((response)=>{
-        response.json();
+        displayInCard(response);
+        // console.log(response);
     })
     .catch((error)=>{
         console.log(error);
     })
-
 })
+
+let container = document.getElementById("search-page-container");
+
+function displayInCard(data){
+    data.forEach(element => {
+        let card = document.createElement("div");
+        let name = document.createElement("h3");
+        let breweryType = document.createElement("p");
+        let city = document.createElement("p");
+        let state = document.createElement("p");
+        name.innerText = element.name;
+        breweryType.innerText = element.brewery_type;
+        city.innerText = element.city;
+        state.innerText = element.state;
+        card.setAttribute('id',card);
+        card.append(name,breweryType,city,state);
+        container.append(card);
+    });
+}
